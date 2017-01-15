82ac83d7ed98911417932aed64babdb90a2ea919
eugenia <orristurmurminintur@gmail.com>
2017-01-15 10:01:27 +0200

    Fix Delete Button

--- src/components/ui/buttons/delete-button.js
+++ src/components/ui/buttons/delete-button.js
@@ -3,13 +3,18 @@ import {Link} from 'react-router';
 import FlatButton from 'material-ui/FlatButton';
 import ActionDelete from 'material-ui/svg-icons/action/delete';
 
-const DeleteButton = ({basePath = '', record = {}, label = 'Видалити'}) => <FlatButton
-  secondary
-  label={label}
-  icon={<ActionDelete />}
-  containerElement={<Link to={`${basePath}/${encodeURIComponent(record.id)}/delete`}/>}
-  style={{overflow: 'inherit'}}
-/>;
+const DeleteButton = ({basePath = '', record = {}, label = 'Видалити'}) => {
+  const includesId = basePath.includes(record.id);
+  const link = includesId ? `${basePath}/delete` : `${basePath}/${encodeURIComponent(record.id)}/delete`;
+
+  return (<FlatButton
+    secondary
+    label={label}
+    icon={<ActionDelete />}
+    containerElement={<Link to={link}/>}
+    style={{overflow: 'inherit'}}
+  />)
+};
 
 DeleteButton.propTypes = {
   basePath: PropTypes.string,
