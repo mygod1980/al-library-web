f18efb3040d90b393575464fa44b40d77e62cf17
eugenia <orristurmurminintur@gmail.com>
2017-02-14 23:02:05 +0200

    Prepare App for Deployment

--- server/app.js
+++ server/app.js
@@ -2,11 +2,21 @@
  * Created by eugenia on 14.02.17.
  */
 'use strict';
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
 
-const app = require('./app');
+const app = express();
 
-const PORT = process.env.PORT || 9000;
+// Setup logger
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+// Always return the main index.html, so react-router render the route in the client
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
+
+module.exports = app;
